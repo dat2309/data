c2dd2e95f779a2fb9ded88c8b0912cb7441a26b0
dat2309 <55149382+dat2309@users.noreply.github.com>
2020-11-11 23:06:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -663,4 +663,5 @@ def display_page(pathname):
 server = app.server
 
 if __name__ == "__main__":
-    app.run_server()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
